d77c06c866b8661fc5b54be16274431c484a00c1
Shambhu <Shambhu>
2017-04-10 17:00:33 +0530

    Initial Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ActionResult response = Navigate() as ActionResult;
+            return response;
+        }
+
+        public ActionResult Navigate()
+        {
+            bool response = true;
+            if (response)
+            {
+                return RedirectToAction("Contact");
+            }
+            else
+            {
+                return RedirectToAction("About");
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
